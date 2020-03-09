f1eac60a031677f02cd87509e8076a7889806612
Rifqi Naufal <rifqigoku2122@gmail.com>
2020-03-09 17:32:15 +0700

    Rifqi Commit 3

--- app/src/main/java/app/gify/co/id/activity/Login.java
+++ app/src/main/java/app/gify/co/id/activity/Login.java
@@ -1,4 +1,18 @@
 package app.gify.co.id.activity;
 
-public class Login {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.gify.co.id.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
 }
