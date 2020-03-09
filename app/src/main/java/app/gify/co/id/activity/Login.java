d8983fc3332985e4cdffce2a0b590da60b2e7732
Firdausfatihul <gakoffical@gmail.com>
2020-03-09 17:09:35 +0700

    a

--- app/src/main/java/app/gify/co/id/activity/Login.java
+++ app/src/main/java/app/gify/co/id/activity/Login.java
@@ -1,4 +1,16 @@
 package app.gify.co.id.activity;
 
-public class Login {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.gify.co.id.R;
+
+public class Login extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
 }
--- app/src/main/java/app/gify/co/id/modal/MadolKado.java
+++ app/src/main/java/app/gify/co/id/modal/MadolKado.java
@@ -1,4 +1,45 @@
 package app.gify.co.id.modal;
 
 public class MadolKado {
+    private int harga;
+    private String nama, kode, gambar;
+
+    public MadolKado(String gambar, int harga, String nama, String kode) {
+        this.gambar = gambar;
+        this.harga = harga;
+        this.nama = nama;
+        this.kode = kode;
+    }
+
+    public String getGambar() {
+        return gambar;
+    }
+
+    public void setGambar(String gambar) {
+        this.gambar = gambar;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
 }
