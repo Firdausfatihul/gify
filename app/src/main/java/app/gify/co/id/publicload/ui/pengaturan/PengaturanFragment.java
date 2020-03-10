bbb27d6b46a7e47b7f8a054e597a38f6d94fd851
Rifqi Naufal <rifqigoku2122@gmail.com>
2020-03-10 22:44:57 +0700

    Rifqi Commit 10

--- app/src/main/java/app/gify/co/id/publicload/ui/pengaturan/PengaturanFragment.java
+++ app/src/main/java/app/gify/co/id/publicload/ui/pengaturan/PengaturanFragment.java
@@ -17,5 +17,6 @@ public class PengaturanFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_pengaturan, container, false);
+
     }
 }
